4d0745ff195998c616427c472a33f2820c90b917
JanoverSaldana <u20201b510@upc.edu.pe>
2025-09-08 07:58:41 -0500

    feat: add banner MetaSoft Solutions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import banner from 'vite-plugin-banner'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      banner(`
+      -----------------------------------------
+      Proyecto: ERP Tracker Mobility
+      Company: MetaSoft Solutions
+      Developers: Janover Gonzalo Saldaña Vela & Claudio Jesús Moreno Rosales 
+      version: 1.0.0
+      Date: Pendiente
+      © 2025 - Todos los derechos reservados
+      -----------------------------------------
+    `)
+
+  ],
 })
