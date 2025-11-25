3c9d0786e2c0a2ee1f117ec818e7c17e852204ca
JanoverSaldana <u20201b510@upc.edu.pe>
2025-11-25 03:24:50 -0500

    feat: Add environment configuration for API base URL and update .gitignore

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -2,9 +2,7 @@
 import axios from "axios";
 import {authenticationInterceptor} from "../../tracker-mobility/security/services/authentication.interceptor.js";
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-const API_BASE_URL = "https://web-service-tracker-mobility-c6uu5.ondigitalocean.app/api/v1"
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const http = axios.create({ baseURL: API_BASE_URL });
 
